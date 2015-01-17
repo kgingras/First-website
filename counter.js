ea86849762248fe7ee6a7e5233df7e980f053e11
Keith <kgingras@umich.edu>
2015-01-17 17:16:42 -0500

    first commit

---
+++ counter.js
@@ -0,0 +1,35 @@
+jQuery(document).ready(function(){
+    // This button will increment the value
+    $('.qtyplus').click(function(e){
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+
+        var $input = $(this).siblings('.qty');
+        
+        var val = parseInt($input.val(), 10);
+        
+        console.log($input.val());
+        $input.val(val + ($(this).hasClass('minus') ? -1 : 1));
+        if (val == 9){
+            alert("GAME OVER!")
+        }
+    });
+    // This button will decrement the value till 0
+    $(".qtyminus").click(function(e) {
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        var $input = $(this).siblings('.qty');
+        
+        var val = parseInt($input.val(), 10);
+        
+        console.log($input.val());
+        if(val > 0){
+            $input.val(val - 1);
+        }
+        else if (!isNaN(currentVal)){
+            $input.val(0);
+        }
+    });
+});
