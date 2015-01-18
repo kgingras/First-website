9cb0e6827f2dbbb1878603b5a9a9e5b48042f773
Keith <kgingras@umich.edu>
2015-01-17 21:59:43 -0500

    update settlers

--- counter.js
+++ counter.js
@@ -15,6 +15,8 @@ jQuery(document).ready(function(){
             alert("GAME OVER!")
         }
     });
+
+
     // This button will decrement the value till 0
     $(".qtyminus").click(function(e) {
         // Stop acting like a button
@@ -31,5 +33,20 @@ jQuery(document).ready(function(){
         else if (!isNaN(currentVal)){
             $input.val(0);
         }
+    });
+
+
+    $('.numplus').click(function(e){
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+
+        var $input = $(this).siblings('.qty');
+        
+        var val = parseInt($input.val(), 10);
+        
+        console.log($input.val());
+        $input.val(val + ($(this).hasClass('minus') ? -1 : 1));
+        
     });
 });
